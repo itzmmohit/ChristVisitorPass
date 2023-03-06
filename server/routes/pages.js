39ae3dc2b2749077c5efd5d3d9beea90b77e47aa
RonBoy <123363908+aaronDev28@users.noreply.github.com>
2023-03-06 11:53:54 +0530

    adminPage v1

---
+++ server/routes/pages.js
@@ -0,0 +1,62 @@
+//this file is for creating the routes for all the pages to be used.
+
+const express = require('express');
+const authController = require('../controllers/auth');
+const router = express.Router();
+
+//change the below urls of router.get to /admin/given_value later
+
+//Node.js callbacks are a special type of function passed as an argument to
+//another function. They're called when the function that contains the callback as an argument completes its execution, and
+//allows the code in the callback to run in the meantime. Callbacks help us make asynchronous calls
+//below is an example of callback function, the (req,res) arrow function.
+
+router.get('/', authController.isLoggedIn, (req,res) => { //This means that whenever we go to "/" page in url, do this function (takes a request and response obj)
+    res.render('index', {
+        user: req.user//same controller given in /profile
+    });  //this will go to index.hbs file
+});
+
+router.get('/register', (req,res) => {
+    res.render('register'); //this will render register.hbs file when we go to /register url
+});
+
+router.get('/login', (req,res) => {
+    res.render('login'); //this will render register.hbs file when we go to /register url
+});
+
+//before going to the profile page, first we run a middleware(the controller function), then run the (req,res) fn
+//This controller fn makes sure that only a logged in user can access the profile page.
+router.get('/profile', authController.isLoggedIn, (req, res) => {
+    //console.log(req.message);//grab var from middleware (authController.isLoggedIn)
+    if(req.user) //after the middleware controller execution, we see if we get a req.user back from it.
+    {
+        res.render('profile', {
+            user: req.user//pass user details to profile page
+        });
+    }else{
+        res.redirect('/login');
+    }
+    
+})
+
+module.exports = router;
+
+//Module exports are the instructions that tell Node. js which bits of code (functions, objects, etc.) to export from a
+// given file so that other files are allowed to access the exported code. In this case, the object is router.
+
+
+
+
+
+
+//The above router code is same as below if defined in app.js:
+
+// app.get("/", (req, res) => { 
+//     res.render('index'); 
+// })
+
+// app.get("/register", (req, res) => {
+//     res.render('register'); 
+// })
+
