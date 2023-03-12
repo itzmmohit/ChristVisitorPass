75c25ec6aaf3bc57956828f8abd5fb2a53ec94d2
itzmmohit <mohit.mehta@mca.christuniversity.in>
2023-03-12 17:38:51 +0530

    Change

--- Client/dist/script.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function showDiv() {
-  document.getElementById('img').style.display = "block";
-  document.getElementById('cbut').style.display = "none";
-  document.getElementById('sub').style.display = "none";
-}
--- Client/script.js
+++
@@ -1,6 +0,0 @@
-function showDiv() {
-    document.getElementById('img').style.display = "block";
-    document.getElementById('cbut').style.display = "none";
-    document.getElementById('sub').style.display = "none";
-}
-
